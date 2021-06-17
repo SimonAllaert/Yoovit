0cc84925ad999bc99830f43bb8082b23127f6d6b
SimonAllaert <simonallaert@gmail.com>
2021-06-17 03:19:11 +0200

    route on map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,6 +3,7 @@ import { StyleSheet, View } from 'react-native';
 import MainStatusBar from '../components/MainStatusBar';
 import MainHeader from '../components/MainHeader';
 import MapView, { Marker } from 'react-native-maps';
+//import DIRECTIONS_API_KEY from '../ApiKeys';
 
 const initialRegion = {
   latitude: 51.02246,
@@ -16,20 +17,23 @@ export default class MapScreen extends React.Component {
     super(props)
     this.state={
         locaties:[],
+        routeIsActive: false,
         initialRegion,
     }
 };
 
   componentDidMount() {
-    fetch('http://yoovit.site/api/alle_locaties')
-      .then((response) => response.json())
-      .then((json) => {
-        this.setState({ 
-          locaties: json,
-          initialRegion: initialRegion,
-        });
-      })
-      .catch((error) => console.error(error));
+    if (!this.state.routeIsActive) {
+      fetch('http://yoovit.site/api/alle_locaties')
+        .then((response) => response.json())
+        .then((json) => {
+          this.setState({ 
+            locaties: json,
+            initialRegion: initialRegion,
+          });
+        })
+        .catch((error) => console.error(error));
+    }
   };
 
   mapMarkers = () => {
@@ -45,8 +49,37 @@ export default class MapScreen extends React.Component {
     ></Marker>)
   };
 
+  mapPolylines = () => {
+    if (this.state.routeIsActive) {
+      const mode = 'walking'
+      const origin = this.state.locaties[0].field_locatie_latitude + "%2C" + this.state.locaties[0].field_locatie_longitude;
+      const destination = this.state.locaties[0].field_locatie_latitude + "%2C" + this.state.locaties[0].field_locatie_longitude;
+      //const APIKEY = DIRECTIONS_API_KEY;
+      const waypoints = this.state.locaties[1].field_locatie_latitude + "%2C" + this.state.locaties[1].field_locatie_longitude + "%7C" + this.state.locaties[2].field_locatie_latitude + "%2C" + this.state.locaties[2].field_locatie_longitude;
+      const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${APIKEY}&mode=${mode}&waypoints=${waypoints}`;
+    };
+  };
+
+  activateRoute(newRoute) {
+      this.setState({
+        locaties: newRoute.tussenstops,
+        routeIsActive: true,
+        initialRegion: 
+          {
+            latitude: parseFloat(newRoute.tussenstops[0].field_locatie_latitude),
+            longitude: parseFloat(newRoute.tussenstops[0].field_locatie_longitude),
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05,
+          },
+      });
+  };
+
   render () {
     const {initialRegion} = this.state;
+    const {route} = this.props;
+    if (typeof route.params !== 'undefined' && !this.state.routeIsActive) {
+      this.activateRoute(route.params.selectedRoute);
+    };
 
     return (
       <View style={styles.container}>
@@ -57,6 +90,7 @@ export default class MapScreen extends React.Component {
             initialRegion={initialRegion}
           >
             {this.mapMarkers()}
+            {/*this.mapPolylines()*/}
           </MapView>
       </View>
     );
--- screens/RouteDetailsScreen.js
+++ screens/RouteDetailsScreen.js
@@ -4,25 +4,100 @@ import MainStatusBar from '../components/MainStatusBar';
 import MainHeader from '../components/MainHeader';
 import { useNavigation } from '@react-navigation/native';
 
-const StartRoute = () => {
+const StartRoute = ({item}) => {
+  const navigation = useNavigation();
 
   return (
-    <TouchableOpacity style={styles.button} onPress={() => alert('Work in progress')}>
+    <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Map', {selectedRoute: item})}>
       <Text>Start route</Text>
     </TouchableOpacity>
   );
+};
+
+const DUMMY_ROUTE = 
+{
+  nid: "9",
+  title: "Torpedo Route",
+  body: "Torpedo is voornamelijk gefilmd geweest in de provincie Antwerpen. De route die er kan gevolgd worden wanneer er op Start route gedrukt kan er de plaatsen bezocht worden die gebruikt zijn om torpedo te filmen.",
+  field_lengteroute: "43,6 km",
+  tussenstops:
+    [
+      {
+        tid: "1",
+        name: "Geeneinde 54, 2381 Ravels",
+        description__value: "Torpedo",
+        field_locatie_latitude: "51.3983000000",
+        field_locatie_longitude: "4.9603300000",
+        quiz_vraag: 'Wat werd op deze locatie opgenomen?',
+        quiz_antwoorden:
+          [
+            {
+              ant1: 'De buitenkant v/d duikboot',
+              ant2: 'De interieur opnames v/d duikboot',
+              ant3: 'Geen van beide',
+            },
+          ],
+        sfeerbeelden:
+          [
+            '/sites/default/files/2021-06/torpedo1.jpeg',
+            '/sites/default/files/2021-06/torpedo2.jpeg',
+            '/sites/default/files/2021-06/torpedo3.jpeg',
+            '/sites/default/files/2021-06/torpedo4.jpeg',
+          ],
+      },
+      {
+        tid: "2",
+        name: "Waaiberg 1, 2460 Kasterlee",
+        description__value: "Torpedo",
+        field_locatie_latitude: "51.2339000000",
+        field_locatie_longitude: "4.9732500000",
+        quiz_vraag: '',
+        quiz_antwoorden:
+          [
+            {
+              ant1: '',
+              ant2: '',
+              ant3: '',
+            },
+          ],
+          sfeerbeelden:
+            [
+            ],
+      },
+      {
+        tid: "3",
+        name: "Riststraat 20, 2300 Turnhout",
+        description__value: "Torpedo",
+        field_locatie_latitude: "51.3529000000",
+        field_locatie_longitude: "4.9657100000",
+        quiz_vraag: '',
+        quiz_antwoorden:
+          [
+            {
+              ant1: '',
+              ant2: '',
+              ant3: '',
+            },
+          ],
+          sfeerbeelden:
+            [
+            ],
+      },
+    ],
 };
 
 export default class RouteDetailsScreen extends React.Component {
   constructor(props){
     super(props)
     this.state={
+      selectedRoute: DUMMY_ROUTE,
     };
   };
 
   render () {
-      const{route} = this.props;
-      const selectedRoute = route.params.selectedRoute;
+      //const{route} = this.props;
+      //const selectedRoute = route.params.selectedRoute;
+      const {selectedRoute} = this.state;
 
       return (
         <View style={styles.container}>
